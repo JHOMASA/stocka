07dfecaa88c04411f5a6929d1de4e711862baf2a
jhonatan Salas <128430511+JHOMASA@users.noreply.github.com>
2025-06-17 14:33:07 -0500

    requirements.py

--- requirements.py
+++ requirements.py
@@ -1,4 +1,7 @@
-streamlit==1.22.0
+streamlit==1.45.1
+pandas==2.3.0
 pywhatkit==5.4
-fpdf2==2.7.4
-sqlite3==2.6.0
+fpdf2==2.7.7
+sqlite3==2.6.0  # Usually included in Python standard library
+python-dateutil==2.9.0
+pillow==11.2.1
